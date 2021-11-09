bd730efa921bc975ea68b46031b99991dcce1652
jairajsahgal <jairajiitaspirant@gmail.com>
2021-11-10 01:40:06 +0530

    Completed lecture Signals

--- users/models.py
+++ users/models.py
@@ -1,7 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
-# Create your models here.
 import uuid
+# Create your models here.
+
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
+
 class Profile(models.Model):
     user = models.OneToOneField(User,on_delete=models.CASCADE,null=True,blank=True)
     name = models.CharField(max_length=200,blank=True,null=True)
@@ -20,7 +24,7 @@ class Profile(models.Model):
     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
 
     def __str__(self):
-        return str(self.user.username)
+        return str(self.username)
 
 class Skill(models.Model):
     owner = models.ForeignKey(Profile,on_delete=models.CASCADE,null=True,blank=True)
@@ -31,3 +35,5 @@ class Skill(models.Model):
 
     def __str__(self):
         return str(self.name)
+
+
