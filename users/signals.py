debd3811b750a66daab11ae19f37b67c31e9b4a5
jairajsahgal <jairajiitaspirant@gmail.com>
2021-12-18 00:47:59 +0530

    Completed Sending Emails and Password Reset Module

--- users/signals.py
+++ users/signals.py
@@ -2,6 +2,9 @@ from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 from django.contrib.auth.models import User
 from .models import Profile
+
+from django.core.mail import send_mail
+from django.conf import settings
 # @receiver(signal=post_save,sender=Profile)
 
 
@@ -14,6 +17,17 @@ def createProfile(sender, instance, created, **kwargs):
             username = user.username,
             email=user.email,
             name = user.first_name
+        )
+
+        subject = 'Welcome to DevSearch'
+        message = 'We are glad you came around'
+
+        send_mail(
+            subject,
+            message,
+            settings.EMAIL_HOST_USER,
+            [profile.email],
+            fail_silently=False
         )
 
 def updateUser(sender,instance,created,**kwargs):
